18c5876299cf577c204a6ed57bca55d079580cc9
Logan <theapachewarrior@hotmail.com>
2022-08-18 15:57:42 -0400

    almost done

--- script.js
+++ script.js
@@ -1,116 +1,55 @@
-(function(){
-	function buildQuiz(){
-	  // variable to store the HTML output
-	  const output = [];
+  const names = [
+    "Logen Ninefingers",
+"al'Lan Mandragoran",
+"Jessica Atréides",
+"Anomander Rake",	
+"Jasnah Kholin",
+"Beric Dondarrion",
+"Emhyr var Emreis",
+"Celebrimbor Alfelf",
+"Tattersail The Mighty",
+"Granny Weatherwax",
+"Lydan Amana",
+"Syrin Barlowe",
+"Ptorik Caddel",
+"Joz Bay tree",
+"Varog Fox",
+"Gethrod Free",
+"Hezra Francis",
+"Feron Hart",
+"Ophni Hendri", 
+"Colborn Katz",
+"Fintis Laurier", 
+"Gatlin Little dog",
+"Jinto Madden",
+"Hagalbar Viotto",
+"Krinn Sai",
+"Lenox Villarreal",
+"Abraham	Revvyn",
+"Hodus Boone",
+"Dimian Ashford",
+"Paskel Good",
+"Kontas Elrod",	
+"Weston Fredericks",
+"Azamarr Hale",
+"Jather Langston",	
+"Tekren Levisay",
+"Jareth McElfresh", 
+"Adon Trevino",
+" Viotto Whitlock",	
+"Yarbrough Beergut",
+"Hemm Gentar",
+"Sarkin Lustros",
+"Blaiz Zenner",
+"Talon Dagrod",
+"Agro Baashar",
+"Zagaroth Feston",
+"Turrek Esdel"
+  ]
   
-	  // for each question...
-	  myQuestions.forEach(
-		(currentQuestion, questionNumber) => {
-  
-		  // variable to store the list of possible answers
-		  const answers = [];
-  
-		  // and for each available answer...
-		  for(letter in currentQuestion.answers){
-  
-			// ...add an HTML radio button
-			answers.push(
-			  `<label>
-				<input type="radio" name="question${questionNumber}" value="${letter}">
-				${letter} :
-				${currentQuestion.answers[letter]}
-			  </label>`
-			);
-		  }
-  
-		  // add this question and its answers to the output
-		  output.push(
-			`<div class="question"> ${currentQuestion.question} </div>
-			<div class="answers"> ${answers.join('')} </div>`
-		  );
-		}
-	  );
-  
-	  // finally combine our output list into one string of HTML and put it on the page
-	  quizContainer.innerHTML = output.join('');
-	}
-  
-	function showResults(){
-  
-	  // gather answer containers from our quiz
-	  const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-	  // keep track of user's answers
-	  let numCorrect = 0;
-  
-	  // for each question...
-	  myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-		// find selected answer
-		const answerContainer = answerContainers[questionNumber];
-		const selector = `input[name=question${questionNumber}]:checked`;
-		const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-		// if answer is correct
-		if(userAnswer === currentQuestion.correctAnswer){
-		  // add to the number of correct answers
-		  numCorrect++;
-  
-		  // color the answers green
-		  answerContainers[questionNumber].style.color = 'lightgreen';
-		}
-		// if answer is wrong or blank
-		else{
-		  // color the answers red
-		  answerContainers[questionNumber].style.color = 'red';
-		}
-	  });
-  
-	  // show number of correct answers out of total
-	  resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-	}
-  
-	const quizContainer = document.getElementById('quiz');
-	const resultsContainer = document.getElementById('results');
-	const submitButton = document.getElementById('submit');
-	const myQuestions = [
-	  {
-		question: "Who invented JavaScript?",
-		answers: {
-		  a: "Douglas Crockford",
-		  b: "Sheryl Sandberg",
-		  c: "Brendan Eich"
-		},
-		correctAnswer: "c"
-	  },
-	  {
-		question: "Which one of these is a JavaScript package manager?",
-		answers: {
-		  a: "Node.js",
-		  b: "TypeScript",
-		  c: "npm"
-		},
-		correctAnswer: "c"
-	  },
-	  {
-		question: "Which tool can you use to ensure code quality?",
-		answers: {
-		  a: "Angular",
-		  b: "jQuery",
-		  c: "RequireJS",
-		  d: "ESLint"
-		},
-		correctAnswer: "d"
-	  }
-	];
-  
-	// Kick things off
-	buildQuiz();
-  
-	// Event listeners
-	submitButton.addEventListener('click', showResults);
-  })();
+  const random = Math.floor(Math.random() * names.length);
+  const Namesmath = Math.floor(Math.random() * names.length);
 
-  var audio = document.getElementById("myaudio");
-  audio.volume = 0.1;
-  
+  function displayMessage(){
+    document.getElementById("msg").innerHTML = "Ah yes we see it now! you have an adventurers heart! from now on you shall be known as " + (Namesmath, names[random]) + " hold tight to your friends, for you never know how many helping hands you are from oblivion!";
+  }
